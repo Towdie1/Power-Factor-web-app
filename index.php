e9e6f2860af82f492d8c753894b2deee2c061fdb
Towdie <31443504+Towdie1@users.noreply.github.com>
2017-10-01 17:12:53 +0100

    with php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php  
+        //Only process the form if $_POST isn't empty
+        if ( ! empty ($_POST )){
+           
+
+        //connect to MySQL
+        $mysqli = new mysqli('localhost', 'towdie', '!Sp4nk1ngs?', 'pf_data')
+        
+        // check the connection
+        if ( $mysqli->connect_error) {
+            die('Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error );
+        }
+        
+        // Insert our data
+        $sql = "INSERT INTO members (name) 
+        VALUES ('{$mysqli->real_escape_string($_POST['name'])}'";
+        $insert = $mysqli->query($sql);
+
+        //Print response from MySQL
+        if ($insert) {
+            echo "Success! Row ID: {$mysqli->insert_id}";
+        } else {
+            die("error: {$mysqli->errno} : {$mysqli->error}");
+        }
+
+        //Close our connection
+        
+        $mysqli->close(;)
+    }
+
+        ?>
