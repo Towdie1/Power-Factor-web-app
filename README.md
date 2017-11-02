6621d0f35f21a45fffa552abaaa62043e88a1e20
Towdie <31443504+Towdie1@users.noreply.github.com>
2017-11-02 09:34:34 +0000

    delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# Power-Factor-web-app
-
-An app to record and chart your weight training progress using the power factor method.
--- scripts.js
+++
@@ -1,7 +0,0 @@
-function changeText(id){ 
-    var weight = document.getElementById('weight-input').value;
-    var reps = document.getElementById('reps-input').value;
-    var time = document.getElementById('time-input').value;
-     
-     document.getElementById(id).value = ((reps*weight)/time)+" kg per minute";
-}
